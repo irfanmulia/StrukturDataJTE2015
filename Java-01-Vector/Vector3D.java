506dae90038d9ec3a70ff142c15b5b9100e39e5a
irfanmulia <12fanmulia@gmail.com>
2015-09-19 17:03:28 +0700

    Praktikum Vector

---
+++ Java-01-Vector/Vector3D.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Vector3D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Date;
+
+public class Vector3D
+{
+    double x, y, z;
+    public Vector3D(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getZ(){
+        return z;
+    }
+}
